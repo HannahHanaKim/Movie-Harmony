a2326d623c48d5220879981eda67b19511f12aca
Hannah Kim <hannahhanakim@gmail.com>
2018-06-08 04:09:00 +0900

    Activities added
    image button added

--- app/src/main/java/com/example/hannahkim/movieharmony/SelectTheater.java
+++ app/src/main/java/com/example/hannahkim/movieharmony/SelectTheater.java
@@ -2,12 +2,19 @@ package com.example.hannahkim.movieharmony;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class SelectTheater extends AppCompatActivity {
+
+    ImageButton cgvBtn, megaboxBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_theater);
+
+        cgvBtn = (ImageButton)findViewById(R.id.cgvBtn);
+        megaboxBtn = (ImageButton)findViewById(R.id.megaboxBtn);
+
     }
 }
